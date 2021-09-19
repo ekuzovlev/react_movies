9aea80cb1eb544852575b4b98c26aa82f62c8836
kuz-en <bio19@ya.ru>
2021-09-19 10:11:18 +0300

    Movies list

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,30 @@
-function Main() {
-  return <main className='container content'>
-    Hello from Movies App
-  </main>
+
+import React from "react"
+import {Movies} from '../components/Movies'
+import {Preloader} from '../components/Preloader'
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  }
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=72caea68&s=matrix')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
+
+  render() {
+    const {movies} = this.state;
+
+    return <main className='container content'>
+      {
+        movies.length ? (
+          <Movies movies={this.state.movies} />
+        ) : <Preloader />
+      }
+    </main>
+  }
 }
 
 export {Main}
