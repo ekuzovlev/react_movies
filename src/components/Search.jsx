824a6c09f6ee1f119c78b2cff047fb1e2f043f59
kuz-en <bio19@ya.ru>
2021-09-19 15:26:43 +0300

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,11 @@ class Search extends React.Component {
     search: '',
   }
 
-
+handleKey = (event) => {
+  if (event.key === 'Enter') {
+    this.props.searchMovies(this.state.search);
+  }
+}
   render() {
     return <div className="row">
       <div className="input-field">
@@ -14,8 +18,10 @@ class Search extends React.Component {
           placeholder="Search"
           type="search"
           value={this.state.search}
-          onChange={(e) => this.setState({search: e.target.value})}
+          onChange={(e) => {this.setState({search: e.target.value})}}
+          onKeyDown={this.handleKey}
         />
+        <button className="btn search-btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
       </div>
     </div>
 
