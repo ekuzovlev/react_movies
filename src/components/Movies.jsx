b7e68616ce5b489ce0dbf454ac8be6cc8203f246
kuz-en <bio19@ya.ru>
2021-09-19 23:12:07 +0300

    Project completed

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,16 @@
-import {Movie} from './Movie'
+import { Movie } from './Movie';
 
-function Movies (props) {
-  const {movies} = props;
+function Movies(props) {
+    const { movies = [] } = props;
 
-    return <div className="movies">
-      {movies.map(movie => (
-        <Movie key={movie.imdbID}{ ...movie} />
-      ))}
-    </div>;
+    return (
+        <div className='movies'>
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+        </div>
+    );
 }
-
 export { Movies };
